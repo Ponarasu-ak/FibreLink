683e91e838fa6fc35d1f9d7cc571b78d51dc8ab1
Ponarasu-ak <ponarasu.ak@gmail.com>
2025-01-08 19:39:44 +0530

    no message

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold text-emerald-400 underline">
-  Hello world!
+  return <h1 className="text-3xl font-bold text-blue-400 underline">
+ fibrelink
 </h1>;
 }
 
